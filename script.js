443b287cf50a3c3e818c91efb07d37572f1ce70f
Kyle Ziulkowski <kziulkowski@nexient.com>
2021-11-01 12:01:38 -0700

    update UI

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = () => {
                     const longitude = place.location.lng;
 
                     // add place name
-                    const placeText = document.createElement('a-link', { is : 'placeText' });
+                    const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
