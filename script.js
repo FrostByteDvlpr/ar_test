0a41077cafb5c465307372ac92e158d633379362
unknown <kyleziulkowski@gmail.com>
2021-11-01 12:49:00 -0400

    enhance location beacon

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = () => {
                     const longitude = place.location.lng;
 
                     // add place name
-                    const placeText = document.createElement('a-link');
+                    const placeText = document.createElement('a-box');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
